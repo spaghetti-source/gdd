1809ef289f9596be0b7e683c5c7204849e5234a3
Takanori Maehara <maehara@prefield.com>
2018-07-19 18:45:13 +0900

    lights out

--- example/lightsout.cc
+++ example/lightsout.cc
@@ -10,25 +10,19 @@
 using namespace std;
 
 int main() {
-  int width = 4, turn = width*width;
-  int n = 2*width*width;
+  int width, height; 
+  cin >> width >> height;
+  int turn = width*height;
+  int n = 2*width*height;
   vector<Permutation> gen, elem;
-
-  /*
-  for (int i = 0; i < n; ++i) {
-    for (int j = i+1; j < n; ++j) {
-      gen.push_back(Permutation::transposition(n, i, j));
-    }
-  }
-  */
 
   int dx[] = {0,1,0,-1,0}, dy[] = {0,0,-1,0,1};
   for (int i = 0; i < width; ++i) {
-    for (int j = 0; j < width; ++j) {
+    for (int j = 0; j < height; ++j) {
       Permutation g(n);
       for (int k = 0; k < 5; ++k) {
         int x = i+dx[k], y = j+dy[k];
-        if (x < 0 || y < 0 || x >= width || y >= width) continue;
+        if (x < 0 || y < 0 || x >= width || y >= height) continue;
         g *= Permutation::transposition(n, x+width*y, x+width*y+turn);
       }
       elem.push_back(g);
@@ -43,9 +37,9 @@ int main() {
     z = gdd.cup(z, gdd.singleton(elem[i]));
   }
   int w = gdd.top;
+  cout << "cardinality | number of nodes | total nodes" << endl;
   for (int k = 0; k < 20; ++k) {
     w = gdd.cartesianProduct(z, w);
     cout << gdd.cardinality(w) << " | " << gdd.numberOfNodes(w) << " | " << gdd.node.size() << endl;
   }
-
 }
